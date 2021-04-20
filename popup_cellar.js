1d21c4b224a6a531c11e8c6903f5545299b0f145
Maphias <55281567+Maphias@users.noreply.github.com>
2021-04-20 19:13:49 +0000

    Deploying to gh-pages from master @ b89bf2a8aecbdae2f6ee28109a9a2718e2257b4a 🚀

--- popup_cellar.js
+++ popup_cellar.js
@@ -1,2 +1,5 @@
 WA.sendChatMessage('Willkommen im Keller! In den Räumen sind ein paar Videos verteilt. Schaut euch doch mal um!', 'Mr Robot');
 
+WA.onEnterZone('popuplevitron', () => {
+    WA.openCoWebSite('https://maphias.github.io/girls-day-2021-physik/textlevitron.html');
+})
