543e0ef9c3c159ea6d0ad6aa1f7574fc0bd74418
Matthias Frerichs <matthiasfrerichs@gmx.net>
2021-04-20 22:59:39 +0200

    stuff

--- popup_main_room.js
+++ popup_main_room.js
@@ -1,1 +1,6 @@
 WA.sendChatMessage('Willkommen zum Girls Day der Fakultät für Physik Würzburg', 'Mr Robot');
+
+
+WA.onEnterZone('timeschedule', () => {
+    WA.openCoWebSite('https://maphias.github.io/girls-day-2021-physik/websites/timeschedule.html');
+})
